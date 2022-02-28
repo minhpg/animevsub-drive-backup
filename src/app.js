4ea96f494bb752203abe317d184141416881e287
minhpg <67231681+minhpg@users.noreply.github.com>
2022-02-28 17:56:03 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ mongoose.connect(process.env.MONGO_DB || 'mongodb://127.0.0.1/drive-backup')
 app.use('/api', (req, res, next) => {
     const key = req.query.key
     if(process.env.API_KEY){
-        if (key != process.env.API_KEY) res.status(404)
+        if (key != process.env.API_KEY) {
+            res.status(404)
+            res.end()
+        }
         else {
             next()
         }
