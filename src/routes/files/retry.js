fb11017d1fb32cc02254b611386010056d500b9c
minhpg <67231681+minhpg@users.noreply.github.com>
2022-02-28 16:00:26 +0700

    commit

--- src/routes/files/retry.js
+++ src/routes/files/retry.js
@@ -3,24 +3,23 @@ const sharedDriveLib = require('../../functions/sharedDrive')
 const uploadFileJob = require('../../queues/files')
 module.exports = async (req, res) => {
     try {
-        const file_id = req.params.file_id
+        let file_id = req.params.file_id
         const file = await fileSchema.findOne({ origin: file_id }).exec()
         if (!file) throw new Error('file does not exist!')
         const shared_drive = await sharedDriveLib.get()
         const parent_id = shared_drive.id
-        let origin_id = file_id
         if (file.backups) {
             for (backup of file.backups) {
-                if (backup.type == 'txt') origin_id = backup.id
+                if (backup.type == 'txt') file_id = backup.id
             }
         }
-        console.log(`backing up from ${origin_id}`)
-        await uploadFileJob({ origin_id, parent_id })
+        console.log(`backing up from ${file_id}`)
+        await uploadFileJob({ file_id, parent_id })
         await file.updateOne({ error: false, error_message: null }).exec()
         res.json({
             success: true,
             data: {
-                id: file_id,
+                id: file.id,
                 parent: parent_id
             }
         })
