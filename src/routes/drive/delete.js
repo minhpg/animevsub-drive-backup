e6c76df8a4ef4231259c4e10783de8ca06fecb45
minhpg <67231681+minhpg@users.noreply.github.com>
2022-02-28 23:08:59 +0700

    commit

--- src/routes/drive/delete.js
+++ src/routes/drive/delete.js
@@ -3,16 +3,9 @@ const sharedDriveLib = require('../../functions/sharedDrive')
 module.exports = async (req, res) => {
     try {
         const shared_drive_id = req.params.shared_drive_id
-        const shared_drive = await sharedDriveLib.find(shared_drive_id)
+        const shared_drive = await sharedDriveLib.delete(shared_drive_id)
         res.json({
             success: true,
-            data: {
-                id: shared_drive_id,
-                file_count: shared_drive.file_count,
-                disabled: shared_drive.disabled,
-                error: shared_drive.error,
-                error_message: shared_drive.error_message
-            }
         })
     }
 
