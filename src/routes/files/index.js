a1a13af6ad468ad935a734baa65c8946bd248847
minhpg <67231681+minhpg@users.noreply.github.com>
2022-02-28 14:20:55 +0700

    commit

--- src/routes/files/index.js
+++ src/routes/files/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 router.get('/create/:file_id', require('./create'))
 router.get('/get/:file_id', require('./get'))
+router.get('/retry/:file_id', require('./retry'))
 router.get('/delete/:file_id', require('./delete'))
 router.get('/list', require('./list'))
 
