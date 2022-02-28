177ef1f62ed08c797131d6a8bb58f4b713f7ff30
minhpg <67231681+minhpg@users.noreply.github.com>
2022-02-28 14:09:29 +0700

    commit

--- src/functions/files/index.js
+++ src/functions/files/index.js
@@ -1,23 +1,35 @@
 const upload = require('./upload')
-const download = require('./download')
+const download = require('../../google-drive-api/download')
 const changeChecksum = require('./changeChecksum')
+const uuid = require('uuid')
+const fs = require('fs')
 module.exports = async (drive, fileId, parent_folder) => {
-    console.log(`copying ${fileId} to ${parent_folder}`)
-    const time = Date.now()
+    const filename = uuid.v4()
 
-    const file = await download(drive, fileId)
-    const name = `${fileId}-${time}`
+    try {
+        console.log(`copying ${fileId} to ${parent_folder}`)
+        const time = Date.now()
     
-    await changeChecksum(file)
-    const copyMP4 = upload(drive, fileId, parent_folder, file, name + '.mp4')
-
-    await changeChecksum(file)
-    const copyTxt = upload(drive, fileId, parent_folder, file, name + '.txt')
-
-    const responses = await Promise.all([copyMP4, copyTxt])
-
-    return {
-        mp4: responses[0],
-        txt: responses[1]
+        const file = await download(drive, fileId, filename)
+        const name = `${fileId}-${time}`
+        
+        await changeChecksum(file)
+        const copyMP4 = upload(drive, fileId, parent_folder, file, name + '.mp4')
+    
+        await changeChecksum(file)
+        const copyTxt = upload(drive, fileId, parent_folder, file, name + '.txt')
+    
+        const responses = await Promise.all([copyMP4, copyTxt])
+    
+        await fs.promises.unlink(filename)
+        return {
+            mp4: responses[0],
+            txt: responses[1]
+        }
+    
+    }
+    catch(err) {
+        await fs.promises.unlink(filename)
+        throw err
     }
   }
